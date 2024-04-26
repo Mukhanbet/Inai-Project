85c9683b0801667d29998e1cd33918021cb6e13b
Nurmukhanbet <nurmuhanbet2004@icloud.com>
2024-04-26 20:36:36 +0600

    Initial code of the project

---
+++ src/main/java/com/example/InaiProject/InaiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.InaiProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InaiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InaiProjectApplication.class, args);
+	}
+
+}
